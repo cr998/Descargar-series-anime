976d9630ea17040f3b8e61515186057d62dc1501
cr998 <cr998@DESKTOP-173HQPN>
2016-05-24 07:53:50 +0200

    creando interface graffica

--- src/descargaseries/Descarga.java
+++ src/descargaseries/Descarga.java
@@ -5,12 +5,14 @@
  */
 package descargaseries;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -30,10 +32,14 @@ public class Descarga extends Thread {
 
     private String url;
     private int percent;
+    private String name;
+    private String ruta;
+    private DescargaStatus g;
 
     public Descarga(String url) {
         this.url = url;
-        this.percent=0;
+        this.percent = 0;
+        g=
     }
 
     @Override
@@ -57,66 +63,65 @@ public class Descarga extends Thread {
     }
 
     private String enlaceNormal(String url) {
-        String endes="null";
+        String endes = "null";
         try {
-            URL link=new URL(url);
+            URL link = new URL(url);
             InetAddress address = InetAddress.getByName(link.getHost());
             String ip = address.getHostAddress();
-            
 
             Socket sc = new Socket(ip, 80);
-            String peticion = "GET "+link.getFile()+" HTTP/1.1\n";
+            String peticion = "GET " + link.getFile() + " HTTP/1.1\n";
             peticion += "Accept: */*\n";
             peticion += "Accept-Language: es\n";
             peticion += "Accept-Encoding: gzip, deflate\n";
             peticion += "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36\n";
-            peticion += "Host: "+link.getHost()+"\n";
+            peticion += "Host: " + link.getHost() + "\n";
             peticion += "Connection: Keep-Alive\n\n";
             DataOutputStream salida = new DataOutputStream(sc.getOutputStream());
             salida.write(peticion.getBytes());
-            DataInputStream entrada =new DataInputStream(sc.getInputStream());
-            InputStreamReader isr =new InputStreamReader(entrada);
-            BufferedReader br=new BufferedReader(isr);
-            String linea=br.readLine();
-            while(!linea.contains("Location")){
-                linea=br.readLine();
+            DataInputStream entrada = new DataInputStream(sc.getInputStream());
+            InputStreamReader isr = new InputStreamReader(entrada);
+            BufferedReader br = new BufferedReader(isr);
+            String linea = br.readLine();
+            while (!linea.contains("Location")) {
+                linea = br.readLine();
             }
-            
-            endes=linea.split(" ")[1];
+
+            endes = linea.split(" ")[1];
             br.close();
             isr.close();
             entrada.close();
             salida.close();
-            
-            URL url2=new URL(endes);
+
+            URL url2 = new URL(endes);
             URLConnection con = url2.openConnection();
             con.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36");
             con.connect();
-            String longitud=con.getHeaderField("Content-Length");
-            InputStream is= con.getInputStream();
-            FileOutputStream fos = new FileOutputStream("1.mp4");
-            byte [] array = new byte[100];
-            int i=0;
-            int leido = is.read(array);
-            int porcent=-1;
-            while (leido > 0) {
-               fos.write(array,0,leido);
-               leido=is.read(array);
-               i++;
-               if((long) i*array.length*100/Integer.parseInt(longitud)!=porcent){
-                   porcent=(int) i*array.length*100/Integer.parseInt(longitud);
-                   this.percent=porcent;
-               }
-               
+            String longitud = con.getHeaderField("Content-Length");
+            String filename = con.getHeaderField("Content-Disposition");
+            BufferedInputStream in = new BufferedInputStream(con.getInputStream());
+            File fil = new File(filename.split("\"")[1]);
+            this.name = fil.getName();
+            System.out.println(name+" -> "+(Integer.parseInt(longitud)/1048576));
+            fil.createNewFile();
+            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(fil));
+
+            int b;
+            int leido=0;
+            while ((b=in.read())!=-1) {
+                out.write(b);
+                leido++;
             }
             
-            
+            in.close();
+            out.close();
+
         } catch (UnknownHostException ex) {
             Logger.getLogger(Descarga.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(Descarga.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return endes;
     }
 }
