583173e2a4618f026b72a5c46b29014854e36391
alumno <alumno@PC15>
2016-05-25 13:11:02 +0200

    añadiendo scanner de series

--- src/descargaseries/Serie.java
+++ src/descargaseries/Serie.java
@@ -6,12 +6,10 @@
 package descargaseries;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 /**
@@ -22,26 +20,40 @@ public class Serie {
 
     private String name;
     private String urlcapitulo[];
-    private int duracion;
-    String url;
+    private int capitulos;
+    private String url;
+    private String[] categorias;
 
     public Serie(String url) {
         this.url = url;
-        Document doc;
+        getData();
+
+    }
+
+    public void descargar() {
+
+        if (urlcapitulo == null) {
+            System.out.println("olle, no has obtenido los datos antes de descargar");
+        } else {
+            for (int i = 0; i < urlcapitulo.length; i++) {
+                new Descarga(urlcapitulo[i]).run();
+            }
+        }
+
+    }
+
+    private void getData() {
         try {
-            doc = Jsoup.connect(url).userAgent("mozilla").get();
+            Document doc = Jsoup.connect(url).userAgent("mozilla").get();
 
             Elements vector = doc.getElementsByClass("serie-capitulos__list__item");
             urlcapitulo = new String[vector.size()];
+            capitulos = urlcapitulo.length;
 
             for (int i = 0; i < vector.size(); i++) {
                 urlcapitulo[vector.size() - i - 1] = (String) vector.get(i).child(1).attr("href");
             }
 
-            Thread a = new Descarga(urlcapitulo[1]);
-            a.start();
-
-            duracion = urlcapitulo.length;
             //name=doc.getElementsByClass("serie-header__title").html();
 //            Elements vector=doc.getElementsByClass("serie-capitulos__list__item");
 //            for (int i = 0; i < vector.toArray().length; i++) {
@@ -50,7 +62,6 @@ public class Serie {
         } catch (IOException ex) {
             Logger.getLogger(Serie.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
 
 }
